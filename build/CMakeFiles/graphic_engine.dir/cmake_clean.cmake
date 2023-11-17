617c410c759e9935130837cae949bc34b8e9d045
PinkShirmp <nminhtue094@gmail.com>
2023-11-17 20:27:50 +0700

    Made a wwindow pop up!

---
+++ build/CMakeFiles/graphic_engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphic_engine.dir/src/main.cpp.o"
+  "CMakeFiles/graphic_engine.dir/src/main.cpp.o.d"
+  "graphic_engine"
+  "graphic_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphic_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/graphic_engine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for graphic_engine.
+# This may be replaced when dependencies are built.
